db4bd2ddefd914897294bddba4ffd3c46ad9c76d
dhana0208 <techdhana33@gmail.com>
2023-09-29 19:10:11 -0500

    changes for location,position type,employer,corecompetency.

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -13,6 +13,12 @@ public class Location {
         nextId++;
     }
 
+    public Location(String value) {
+        this();
+        this.value=value;
+
+    }
+
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
@@ -51,4 +57,7 @@ public class Location {
         this.value = value;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 }
